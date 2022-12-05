03f0914c1b710eb7c9f86796548719e626ccbca0
thenormalboy123 <91535744+thenormalboy123@users.noreply.github.com>
2022-12-05 16:28:03 +0800

    Add files via upload

---
+++ day4/day4_1.py
@@ -0,0 +1,15 @@
+import re
+
+if __name__ == '__main__':
+    file1 = open('input.txt', 'r')
+    Lines = file1.readlines()
+    count = 0
+
+    for line in Lines: 
+        line_tochange = re.split(r'[-,]', line.strip())
+        if ( (int(line_tochange[0]) >= int(line_tochange[2])) and (int(line_tochange[1]) <= int(line_tochange[3])) ): 
+            count+=1
+        elif ( (int(line_tochange[0]) <= int(line_tochange[2])) and (int(line_tochange[1]) >= int(line_tochange[3])) ):
+            count+=1
+    print(count)
+
